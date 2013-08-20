3aa076da823f0599fbc27efb9a367e7bf30ed241
nicolas <n.chaulet@gmail.com>
2013-08-20 13:30:20 +0200

    add test for google geocoder

--- test/geocoder/googlegeocoder.js
+++ test/geocoder/googlegeocoder.js
@@ -5,6 +5,7 @@
         sinon = require('sinon');
 
     var GoogleGeocoder = require('../../lib/geocoder/googlegeocoder.js');
+    var HttpAdapter = require('../../lib/httpadapter/httpadapter.js');
 
     var mockedHttpAdapter = {
         get: function() {}
@@ -63,6 +64,32 @@
 
             });
 
+            it('Should return geocoded adress', function(done) {
+
+
+
+                var googleAdapter = new GoogleGeocoder(new HttpAdapter());
+
+                googleAdapter.geocode('1 champs élysée Paris', function(err, results) {
+                    err.should.to.equal(false);
+                    results[0].should.to.deep.equal({
+                        "latitude": 48.869261,
+                        "longitude": 2.3091644,
+                        "country": "France",
+                        "city": "Paris",
+                        "zipcode": "75008",
+                        "streetName": "Champs-Élysées",
+                        "streetNumber": "1",
+                        "countryCode": "FR"
+                    });
+
+                    done();
+                });
+
+
+
+            });
+
         });
 
         describe('#reverse' , function() {
@@ -77,6 +104,25 @@
 
                 mock.verify();
 
+            });
+
+            it('Should return geocoded adress', function(done) {
+                var googleAdapter = new GoogleGeocoder(new HttpAdapter());
+                googleAdapter.reverse(40.714232,-73.9612889, function(err, results) {
+                        err.should.to.equal(false);
+                        results[0].should.to.deep.equal({
+                            "latitude": 40.714232,
+                            "longitude": -73.9612889,
+                            "country": "United States",
+                            "city": "Brooklyn",
+                            "zipcode": "11211",
+                            "streetName": "Bedford Avenue",
+                            "streetNumber": "277",
+                            "countryCode": "US"
+                        });
+
+                        done();
+                });
             });
         });
 
