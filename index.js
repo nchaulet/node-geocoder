4eadd0a09f9066c4d85bd119b007af0c2f173a62
nchaulet <n.chaulet@gmail.com>
2013-10-20 20:26:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 
 	var GeocoderFactory = require('./lib/geocoderfactory.js');
 
-	module.exports = function(geocoderName, adapterName) {
-
-		return GeocoderFactory.getGeocoder(geocoderName, adapterName);
+	module.exports = 
+	
+	return GeocoderFactory.getGeocoder(geocoderName, adapterName);
 	};
 })();
