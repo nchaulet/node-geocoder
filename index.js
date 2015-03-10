8334fae0bc09a28119b90c0bbb10669cb5a1c3e3
Nicolas Chaulet <n.chaulet@gmail.com>
2015-03-10 21:56:29 +0100

    fix CS and new syntax for getGeocoder

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 (function() {
 	var GeocoderFactory = require('./lib/geocoderfactory.js');
 
-	module.exports =  GeocoderFactory;
+    var Exports = GeocoderFactory.getGeocoder.bind(GeocoderFactory);
+
+
+    // Should be deprecated in 3.0 @deprecated
+    Exports.getGeocoder = GeocoderFactory.getGeocoder.bind(GeocoderFactory);
+
+	module.exports =  Exports;
 })();
