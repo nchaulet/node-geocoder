8c0547ef8a551fd84f639a2828be1b42172e606d
Pascal Bihler <pascal.bihler@qeevee.com>
2015-03-19 14:00:47 +0100

    Added third callback parameter "rawData"

--- test/geocoder/googlegeocoder.js
+++ test/geocoder/googlegeocoder.js
@@ -161,7 +161,7 @@
                 );
                 var googleAdapter = new GoogleGeocoder(mockedHttpAdapter);
 
-                googleAdapter.geocode('1 champs élysées Paris', function(err, results) {
+                googleAdapter.geocode('1 champs élysées Paris', function(err, results, rawResults) {
                     err.should.to.equal(false);
                     results[0].should.to.deep.equal({
                         "latitude"    : 37.386,
@@ -175,6 +175,25 @@
                         "state"       : "Île-de-France",
                         "stateCode"   : "IDF"
                     });
+
+                    rawResults.should.deep.equal({ status: "OK", results: [{
+                        geometry: {location : {
+                            lat: 37.386,
+                            lng: -122.0838
+                        }},
+                        address_components: [
+                            {types: ['country'], long_name: 'France', short_name: 'Fr' },
+                            {types: ['locality'], long_name: 'Paris' },
+                            {types: ['postal_code'], long_name: '75008' },
+                            {types: ['route'], long_name: 'Champs-Élysées' },
+                            {types: ['street_number'], long_name: '1' },
+                            {types: ['administrative_area_level_1'], long_name: 'Île-de-France', short_name: 'IDF'}
+                        ],
+                        country_code: 'US',
+                        country_name: 'United States',
+                        locality: 'Mountain View',
+                    }]});
+
                     mock.verify();
                     done();
                 });
@@ -186,8 +205,11 @@
 
                 var googleAdapter = new GoogleGeocoder(mockedHttpAdapter);
 
-                googleAdapter.geocode('1 champs élysées Paris', function(err, results) {
+                googleAdapter.geocode('1 champs élysées Paris', function(err, results, rawResults) {
                     err.message.should.to.equal("Status is OVER_QUERY_LIMIT. You have exceeded your rate-limit for this API.");
+
+                    rawResults.should.deep.equal({ status: "OVER_QUERY_LIMIT", error_message: "You have exceeded your rate-limit for this API.", results: [] });
+
                     mock.verify();
                     done();
                 });
@@ -245,7 +267,7 @@
                     }]}
                 );
                 var googleAdapter = new GoogleGeocoder(mockedHttpAdapter);
-                googleAdapter.reverse(40.714232,-73.9612889, function(err, results) {
+                googleAdapter.reverse(40.714232,-73.9612889, function(err, results, rawResults) {
                         err.should.to.equal(false);
                         results[0].should.to.deep.equal({
                             "latitude"    : 40.714232,
@@ -259,6 +281,26 @@
                             "state"       : "État de New York",
                             "stateCode"   : "NY"
                         });
+
+                        rawResults.should.deep.equal({ status: "OK", results: [{
+                            geometry: {location : {
+                                lat: 40.714232,
+                                lng: -73.9612889
+                            }},
+                            address_components: [
+                                {types: ['country'], long_name: 'United States', short_name: 'US' },
+                                {types: ['locality'], long_name: 'Brooklyn' },
+                                {types: ['postal_code'], long_name: '11211' },
+                                {types: ['route'], long_name: 'Bedford Avenue' },
+                                {types: ['street_number'], long_name: '277' },
+                                {types: ['administrative_area_level_1'], long_name: 'État de New York', short_name: 'NY'}
+
+
+                            ],
+                            country_code: 'US',
+                            country_name: 'United States',
+                            locality: 'Mountain View',
+                        }]});
                         mock.verify();
                         done();
                 });
