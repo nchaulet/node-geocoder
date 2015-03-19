7e48be0176e9df147fd385ef9ac8f9a5da12d12c
Pascal Bihler <pascal.bihler@qeevee.com>
2015-03-19 11:26:55 +0100

    Replaced mocked results with real service responses, fixed code to adhere to the tests again.
    Had to replace "Champs-Élysées" by some request returning proper street numbers...

--- test/geocoder/openstreetmapgeocoder.js
+++ test/geocoder/openstreetmapgeocoder.js
@@ -66,38 +66,51 @@
             it('Should return geocoded address', function(done) {
                 var mock = sinon.mock(mockedHttpAdapter);
                 mock.expects('get').once().callsArgWith(2, false, [{
-                        lat: 48.86841815,
-                        lon: 2.30700964746136,
-                        address: {
-                            country_code: 'FR',
-                            country: 'France',
-                            city: 'Paris',
-                            state: '',
-                            postcode: "75008",
-                            road: 'Champs-Élysées',
-                            house_number: "1"
+                        "place_id": "73723099",
+                        "licence": "Data \u00a9 OpenStreetMap contributors, ODbL 1.0. http:\/\/www.openstreetmap.org\/copyright",
+                        "osm_type": "way",
+                        "osm_id": "90394480",
+                        "boundingbox": ["52.5487473", "52.5488481", "-1.8165129", "-1.8163463"],
+                        "lat": "52.5487921",
+                        "lon": "-1.8164307339635",
+                        "display_name": "135, Pilkington Avenue, Castle Vale, Maney, Birmingham, West Midlands, England, B72 1LH, United Kingdom",
+                        "class": "building",
+                        "type": "yes",
+                        "importance": 0.411,
+                        "address": {
+                            "house_number": "135",
+                            "road": "Pilkington Avenue",
+                            "suburb": "Castle Vale",
+                            "hamlet": "Maney",
+                            "city": "Birmingham",
+                            "state_district": "West Midlands",
+                            "state": "England",
+                            "postcode": "B72 1LH",
+                            "country": "United Kingdom",
+                            "country_code": "gb"
                         }
                     }]
                 );
 
                 var osmAdapter = new OpenStreetMapGeocoder(mockedHttpAdapter);
 
-                osmAdapter.geocode('1 champ-élysées Paris', function(err, results) {
+                osmAdapter.geocode('135 pilkington avenue, birmingham', function(err, results) {
+                    mock.verify();
+
                     err.should.to.equal(false);
 
                     results[0].should.to.deep.equal({
-                        "latitude": 48.86841815,
-                        "longitude": 2.30700964746136,
-                        "country": "France",
-                        "city": "Paris",
-                        "state": "",
-                        "zipcode": "75008",
-                        "streetName": "Champs-Élysées",
-                        "streetNumber": "1",
-                        "countryCode": "FR"
+                        "latitude": 52.5487921,
+                        "longitude": -1.8164307339635,
+                        "country": "United Kingdom",
+						            "state": "England",
+                        "city": "Birmingham",
+                        "zipcode": "B72 1LH",
+                        "streetName": "Pilkington Avenue",
+                        "streetNumber": "135",
+                        "countryCode": "GB"
                     });
 
-                    mock.verify();
                     done();
                 });
             });
@@ -108,35 +121,42 @@
             it('Should return geocoded address', function(done) {
                 var mock = sinon.mock(mockedHttpAdapter);
                 mock.expects('get').once().callsArgWith(2, false, {
-                        lat: 40.714232,
-                        lon: -73.9612889,
-                        address: {
-                            country_code: 'US',
-                            country: 'United States',
-                            city: 'Brooklyn',
-                            state: 'New York',
-                            postcode: "11211",
-                            road: 'Bedford Avenue',
-                            house_number: "277"
+                        "place_id": "119109484",
+                        "licence": "Data \u00a9 OpenStreetMap contributors, ODbL 1.0. http:\/\/www.openstreetmap.org\/copyright",
+                        "osm_type": "way",
+                        "osm_id": "279767984",
+                        "lat": "40.714205",
+                        "lon": "-73.9613150927476",
+                        "display_name": "279, Bedford Avenue, Williamsburg, Kings County, NYC, New York, 11211, United States of America",
+                        "address": {
+                            "house_number": "279",
+                            "road": "Bedford Avenue",
+                            "neighbourhood": "Williamsburg",
+                            "county": "Kings County",
+                            "city": "NYC",
+                            "state": "New York",
+                            "postcode": "11211",
+                            "country": "United States of America",
+                            "country_code": "us"
                         }
                     }
                 );
                 var osmAdapter = new OpenStreetMapGeocoder(mockedHttpAdapter);
                 osmAdapter.reverse(40.714232,-73.9612889, function(err, results) {
-                        err.should.to.equal(false);
-                        results[0].should.to.deep.equal({
-                            "latitude": 40.714232,
-                            "longitude": -73.9612889,
-                            "country": "United States",
-                            "state": "New York",
-                            "city": "Brooklyn",
-                            "zipcode": "11211",
-                            "streetName": "Bedford Avenue",
-                            "streetNumber": "277",
-                            "countryCode": "US"
-                        });
-                        mock.verify();
-                        done();
+                    mock.verify();
+                    err.should.to.equal(false);
+                    results[0].should.to.deep.equal({
+                        "latitude": 40.714205,
+                        "longitude": -73.9613150927476,
+                        "country": "United States of America",
+                        "state": "New York",
+                        "city": "NYC",
+                        "zipcode": "11211",
+                        "streetName": "Bedford Avenue",
+                        "streetNumber": "279",
+                        "countryCode": "US"
+                    });
+                    done();
                 });
             });
         });
