060ab4fe112cf99a2bbf3cc193d1d1c0db2b2ed8
Nicolas Chaulet <nicolas.chaulet@busbud.com>
2016-03-27 16:05:07 -0400

    Remove Q and use bluebird instead

--- test/geocoder.js
+++ test/geocoder.js
@@ -1,96 +1,97 @@
-(function() {
-    var chai   = require('chai'),
-        should = chai.should(),
-        expect = chai.expect,
-        sinon  = require('sinon');
+'use strict';
 
-    var Geocoder = require('../lib/geocoder.js'),
-        Q        = require('q');
+var chai   = require('chai');
+var should = chai.should();
+var expect = chai.expect;
+var sinon  = require('sinon');
 
-    var mockedGeocoder = {
-        geocode: function() {},
-        reverse: function() {}
-    };
+var Geocoder = require('../lib/geocoder.js');
 
-    describe('Geocoder', function() {
+var stupidGeocoder = {
+  geocode: function(data, cb) {
+    cb(null, []);
+  },
+  reverse: function(data, cb) {
+    cb(null, []);
+  }
+};
 
-        describe('#constructor' , function() {
+describe('Geocoder', function() {
+  beforeEach(function() {
+    sinon.spy(stupidGeocoder, 'geocode');
+    sinon.spy(stupidGeocoder, 'reverse');
+  });
 
-            it('Should set _geocoder', function() {
+  afterEach(function() {
+    stupidGeocoder.geocode.restore();
+    stupidGeocoder.reverse.restore();
+  });
 
-                var geocoder = new Geocoder(mockedGeocoder);
 
-                geocoder._geocoder.should.be.equal(mockedGeocoder);
-            });
+  describe('#constructor' , function() {
+    it('Should set _geocoder', function() {
+      var geocoder = new Geocoder(stupidGeocoder);
+
+      geocoder._geocoder.should.be.equal(stupidGeocoder);
+    });
+  });
+
+  describe('#geocode' , function() {
+    it('Should call geocoder geocode method', function() {
+      var geocoder = new Geocoder(stupidGeocoder);
+
+      return geocoder.geocode('127.0.0.1')
+        .then(function() {
+          stupidGeocoder.geocode.calledOnce.should.be.true;
         });
-
-        describe('#geocode' , function() {
-            it('Should call mockedGeocoder geocoder method', function() {
-                var mock = sinon.mock(mockedGeocoder);
-                mock.expects('geocode').once().returns({then: function() {}});
-
-                var geocoder = new Geocoder(mockedGeocoder);
-
-                geocoder.geocode('127.0.0.1');
-
-                mock.verify();
-            });
-
-            it('Should return a promise', function() {
-                var geocoder = new Geocoder(mockedGeocoder);
-
-                var promise = geocoder.geocode('127.0.0.1');
-                promise.then.should.be.a('function');
-            });
-        });
-
-        describe('#batchGeocode' , function() {
-            it('Should call mockedGeocoder geocoder method x times', function() {
-                var mock = sinon.mock(mockedGeocoder);
-                mock.expects('geocode').exactly(4).returns({then: function() {}});
-
-                var geocoder = new Geocoder(mockedGeocoder);
-
-                geocoder.batchGeocode([
-                    '127.0.0.1',
-                    '127.0.0.1',
-                    '127.0.0.1',
-                    '127.0.0.1'
-                ]);
-
-                mock.verify();
-            });
-
-            it('Should return a promise', function() {
-                var geocoder = new Geocoder(mockedGeocoder);
-
-                var promise = geocoder.batchGeocode(['127.0.0.1']);
-                promise.then.should.be.a('function');
-            });
-        });
-
-        describe('#reverse' , function() {
-            it('Should call mockedGeocoder reverse method', function() {
-                var mock = sinon.mock(mockedGeocoder);
-                mock.expects('reverse').once().returns({then: function() {}});
-
-                var geocoder = new Geocoder(mockedGeocoder);
-
-                geocoder.reverse(1, 2);
-
-                mock.verify();
-            });
-
-            it('Should return a promise', function() {
-                var geocoder = new Geocoder(mockedGeocoder);
-
-                var promise = geocoder.reverse('127.0.0.1');
-
-                promise.then.should.be.a('function');
-            });
-        });
-
-
     });
 
-})();
+    it('Should return a promise', function() {
+      var geocoder = new Geocoder(stupidGeocoder);
+
+      var promise = geocoder.geocode('127.0.0.1');
+      promise.then.should.be.a('function');
+    });
+  });
+
+  describe('#batchGeocode' , function() {
+    it('Should call stupidGeocoder geocoder method x times', function() {
+      var geocoder = new Geocoder(stupidGeocoder);
+
+      return geocoder.batchGeocode([
+          '127.0.0.1',
+          '127.0.0.1',
+          '127.0.0.1'
+      ]).then(function() {
+        stupidGeocoder.geocode.calledThrice.should.be.true;
+      });
+    });
+
+    it('Should return a promise', function() {
+      var geocoder = new Geocoder(stupidGeocoder);
+
+      var promise = geocoder.batchGeocode(['127.0.0.1']);
+      promise.then.should.be.a('function');
+    });
+  });
+
+  describe('#reverse' , function() {
+    it('Should call stupidGeocoder reverse method', function() {
+      var geocoder = new Geocoder(stupidGeocoder);
+
+      return geocoder.reverse(1, 2)
+        .then(function() {
+          stupidGeocoder.reverse.calledOnce.should.be.true;
+        });
+    });
+
+    it('Should return a promise', function() {
+      var geocoder = new Geocoder(stupidGeocoder);
+
+      var promise = geocoder.reverse('127.0.0.1');
+
+      promise.then.should.be.a('function');
+    });
+  });
+});
+
