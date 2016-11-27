c9476ca7d93e3a17481d829403ebbf759e01beb0
Nicolas Chaulet <n.chaulet@gmail.com>
2016-11-27 12:12:21 -0500

    fix cs test formatter

--- test/formatter/gpxformatter.js
+++ test/formatter/gpxformatter.js
@@ -1,23 +1,21 @@
-var chai = require('chai'),
-    should = chai.should(),
-    expect = chai.expect;
+'use strict';
+var chai = require('chai');
+var expect = chai.expect;
 
 var GpxFormatter = require('../../lib/formatter/gpxformatter.js');
 
 describe('GpxFormatter', function() {
+  describe('#format' , function() {
+    it('should format using gpx format', function() {
+      var formatter = new GpxFormatter();
 
-    describe('#format' , function() {
-        it('should format using gpx format', function() {
-            var formatter = new GpxFormatter();
+      var results = formatter.format([{
+        latitude: 40.714232,
+        longitude: -73.9612889
+      }]);
 
-            var results = formatter.format([{
-                latitude: 40.714232,
-                longitude: -73.9612889
-            }]);
-
-            results.should.be.a('string');
-            results.should.include('<wpt lat="40.714232" lon="-73.9612889"><name></name></wpt>');
-        });
+      results.should.be.a('string');
+      results.should.include('<wpt lat="40.714232" lon="-73.9612889"><name></name></wpt>');
     });
-
+  });
 });
