8d79114e868800a07c9ee3fc526479d8cb211a8a
Nicolas <n.chaulet@gmail.com>
2022-02-13 17:59:02 -0500

    Remove http adapters

--- test/geocoder/openmapquestgeocoder.test.js
+++ test/geocoder/openmapquestgeocoder.test.js
@@ -1,80 +1,87 @@
-(function() {
-    var chai = require('chai'),
-        should = chai.should(),
-        expect = chai.expect,
-        sinon = require('sinon');
+(function () {
+  var chai = require('chai'),
+    should = chai.should(),
+    expect = chai.expect,
+    sinon = require('sinon');
 
-    var MapQuestGeocoder = require('../../lib/geocoder/openmapquestgeocoder.js');
-    var HttpAdapter = require('../../lib/httpadapter/httpadapter.js');
+  var MapQuestGeocoder = require('../../lib/geocoder/openmapquestgeocoder.js');
+  var HttpAdapter = require('../../lib/httpadapter/fetchadapter.js');
 
-    var mockedHttpAdapter = {
-        get: function() {}
-    };
+  var mockedHttpAdapter = {
+    get: function () {}
+  };
 
-    describe('MapQuestGeocoder', () => {
+  describe('MapQuestGeocoder', () => {
+    describe('#constructor', () => {
+      test('an http adapter must be set', () => {
+        expect(function () {
+          new MapQuestGeocoder();
+        }).to.throw(Error, 'OpenMapQuestGeocoder need an httpAdapter');
+      });
 
-        describe('#constructor' , () => {
+      test('an apiKey must be set', () => {
+        expect(function () {
+          new MapQuestGeocoder(mockedHttpAdapter);
+        }).to.throw(Error, 'OpenMapQuestGeocoder needs an apiKey');
+      });
 
-            test('an http adapter must be set', () => {
+      test('Should be an instance of MapQuestGeocoder', () => {
+        var mapquestAdapter = new MapQuestGeocoder(
+          mockedHttpAdapter,
+          'API_KEY'
+        );
 
-                expect(function() {new MapQuestGeocoder();}).to.throw(Error, 'OpenMapQuestGeocoder need an httpAdapter');
-            });
-
-            test('an apiKey must be set', () => {
-
-                expect(function() {new MapQuestGeocoder(mockedHttpAdapter);}).to.throw(Error, 'OpenMapQuestGeocoder needs an apiKey');
-            });
-
-            test('Should be an instance of MapQuestGeocoder', () => {
-
-                var mapquestAdapter = new MapQuestGeocoder(mockedHttpAdapter, 'API_KEY');
-
-                mapquestAdapter.should.be.instanceof(MapQuestGeocoder);
-            });
-
-        });
-
-        describe('#geocode' , () => {
-
-            test('Should not accept IPv4', () => {
-
-                var mapquestAdapter = new MapQuestGeocoder(mockedHttpAdapter, 'API_KEY');
-
-                expect(function() {
-                        mapquestAdapter.geocode('127.0.0.1');
-                }).to.throw(Error, 'OpenMapQuestGeocoder does not support geocoding IPv4');
-
-            });
-
-            test('Should not accept IPv6', () => {
-
-                var mapquestAdapter = new MapQuestGeocoder(mockedHttpAdapter, 'API_KEY');
-
-                expect(function() {
-                        mapquestAdapter.geocode('2001:0db8:0000:85a3:0000:0000:ac1f:8001');
-                }).to.throw(Error, 'OpenMapQuestGeocoder does not support geocoding IPv6');
-
-            });
-
-        });
-
-        describe('#reverse' , () => {
-            test('Should call httpAdapter get method', () => {
-
-                var mock = sinon.mock(mockedHttpAdapter);
-                mock.expects('get').once().returns({then: function() {}});
-
-                var mapquestAdapter = new MapQuestGeocoder(mockedHttpAdapter, 'API_KEY');
-
-                mapquestAdapter.reverse({lat:10.0235,lon:-2.3662});
-
-                mock.verify();
-
-            });
-
-        });
-
-
+        mapquestAdapter.should.be.instanceof(MapQuestGeocoder);
+      });
     });
 
+    describe('#geocode', () => {
+      test('Should not accept IPv4', () => {
+        var mapquestAdapter = new MapQuestGeocoder(
+          mockedHttpAdapter,
+          'API_KEY'
+        );
+
+        expect(function () {
+          mapquestAdapter.geocode('127.0.0.1');
+        }).to.throw(
+          Error,
+          'OpenMapQuestGeocoder does not support geocoding IPv4'
+        );
+      });
+
+      test('Should not accept IPv6', () => {
+        var mapquestAdapter = new MapQuestGeocoder(
+          mockedHttpAdapter,
+          'API_KEY'
+        );
+
+        expect(function () {
+          mapquestAdapter.geocode('2001:0db8:0000:85a3:0000:0000:ac1f:8001');
+        }).to.throw(
+          Error,
+          'OpenMapQuestGeocoder does not support geocoding IPv6'
+        );
+      });
+    });
+
+    describe('#reverse', () => {
+      test('Should call httpAdapter get method', () => {
+        var mock = sinon.mock(mockedHttpAdapter);
+        mock
+          .expects('get')
+          .once()
+          .returns({ then: function () {} });
+
+        var mapquestAdapter = new MapQuestGeocoder(
+          mockedHttpAdapter,
+          'API_KEY'
+        );
+
+        mapquestAdapter.reverse({ lat: 10.0235, lon: -2.3662 });
+
+        mock.verify();
+      });
+    });
+  });
 })();
--- test/geocoder/smartystreetsgeocoder.test.js
+++ test/geocoder/smartystreetsgeocoder.test.js
@@ -1,65 +1,78 @@
-(function() {
-    var chai = require('chai'),
-        should = chai.should(),
-        expect = chai.expect,
-        sinon = require('sinon');
+(function () {
+  var chai = require('chai'),
+    should = chai.should(),
+    expect = chai.expect,
+    sinon = require('sinon');
 
-    var SmartyStreets = require('../../lib/geocoder/smartystreetsgeocoder.js');
-    var HttpAdapter = require('../../lib/httpadapter/httpadapter.js');
+  var SmartyStreets = require('../../lib/geocoder/smartystreetsgeocoder.js');
+  var HttpAdapter = require('../../lib/httpadapter/fetchadapter.js');
 
-    var mockedHttpAdapter = {
-        get: function() {}
-    };
+  var mockedHttpAdapter = {
+    get: function () {}
+  };
 
-    describe('SmartyStreets', () => {
+  describe('SmartyStreets', () => {
+    describe('#constructor', () => {
+      test('an http adapter must be set', () => {
+        expect(function () {
+          new SmartyStreets();
+        }).to.throw(Error, 'SmartyStreets need an httpAdapter');
+      });
 
-        describe('#constructor' , () => {
+      test('an auth-id and auth-token must be set', () => {
+        expect(function () {
+          new SmartyStreets(mockedHttpAdapter);
+        }).to.throw(Error, 'You must specify an auth-id and auth-token!');
+      });
 
-            test('an http adapter must be set', () => {
+      test('Should be an instance of SmartyStreets', () => {
+        var smartyStreetsAdapter = new SmartyStreets(
+          mockedHttpAdapter,
+          'AUTH_ID',
+          'AUTH_TOKEN'
+        );
 
-                expect(function() {new SmartyStreets();}).to.throw(Error, 'SmartyStreets need an httpAdapter');
-            });
-
-            test('an auth-id and auth-token must be set', () => {
-
-                expect(function() {new SmartyStreets(mockedHttpAdapter);}).to.throw(Error, 'You must specify an auth-id and auth-token!');
-            });
-
-            test('Should be an instance of SmartyStreets', () => {
-
-                var smartyStreetsAdapter = new SmartyStreets(mockedHttpAdapter, 'AUTH_ID', 'AUTH_TOKEN');
-
-                smartyStreetsAdapter.should.be.instanceof(SmartyStreets);
-            });
-
-        });
-
-        describe('#geocode' , () => {
-
-            test('Should call httpAdapter get method', () => {
-              var mock = sinon.mock(mockedHttpAdapter);
-              mock.expects('get').withArgs('https://api.smartystreets.com/street-address', {
-                "street": "1 Infinite Loop, Cupertino, CA",
-                "auth-id": "AUTH_ID",
-                "auth-token": "AUTH_TOKEN",
-                "format": "json"
-              }).once().returns({then: function() {}});
-
-              var smartyStreetsAdapter = new SmartyStreets(mockedHttpAdapter, "AUTH_ID", "AUTH_TOKEN");
-
-              smartyStreetsAdapter.geocode("1 Infinite Loop, Cupertino, CA");
-              mock.verify();
-            });
-        });
-
-        describe('#reverse' , () => {
-            test('Should throw expection', () => {
-              var smartyStreetsAdapter = new SmartyStreets(mockedHttpAdapter, 'AUTH_ID', 'AUTH_TOKEN');
-
-                expect(function() {
-                        smartyStreetsAdapter.reverse(10.0235,-2.3662);
-                }).to.throw(Error, 'SmartyStreets doesnt support reverse geocoding!');
-            });
-        });
+        smartyStreetsAdapter.should.be.instanceof(SmartyStreets);
+      });
     });
+
+    describe('#geocode', () => {
+      test('Should call httpAdapter get method', () => {
+        var mock = sinon.mock(mockedHttpAdapter);
+        mock
+          .expects('get')
+          .withArgs('https://api.smartystreets.com/street-address', {
+            street: '1 Infinite Loop, Cupertino, CA',
+            'auth-id': 'AUTH_ID',
+            'auth-token': 'AUTH_TOKEN',
+            format: 'json'
+          })
+          .once()
+          .returns({ then: function () {} });
+
+        var smartyStreetsAdapter = new SmartyStreets(
+          mockedHttpAdapter,
+          'AUTH_ID',
+          'AUTH_TOKEN'
+        );
+
+        smartyStreetsAdapter.geocode('1 Infinite Loop, Cupertino, CA');
+        mock.verify();
+      });
+    });
+
+    describe('#reverse', () => {
+      test('Should throw expection', () => {
+        var smartyStreetsAdapter = new SmartyStreets(
+          mockedHttpAdapter,
+          'AUTH_ID',
+          'AUTH_TOKEN'
+        );
+
+        expect(function () {
+          smartyStreetsAdapter.reverse(10.0235, -2.3662);
+        }).to.throw(Error, 'SmartyStreets doesnt support reverse geocoding!');
+      });
+    });
+  });
 })();
