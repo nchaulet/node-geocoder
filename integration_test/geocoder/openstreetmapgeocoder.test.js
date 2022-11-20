4147ac56bf14f528ed01f22718833f23da0d660f
Nicolas Chaulet <n.chaulet@gmail.com>
2022-11-20 16:02:48 -0500

    Add integration test for openstreetmap geoocder

---
+++ integration_test/geocoder/openstreetmapgeocoder.test.js
@@ -0,0 +1,47 @@
+const NodeGeocoder = require('../../index');
+
+describe('Mapbox geocoder', () => {
+  let geocoder;
+
+  beforeAll(() => {
+    const options = {
+      provider: 'openstreetmap'
+    };
+
+    geocoder = NodeGeocoder(options);
+  });
+
+  describe('geocode', () => {
+    it('works with basic value', async () => {
+      const res = await geocoder.geocode('1231 Av. Lajoie, Montreal');
+
+      expect(res[0]).toBeDefined();
+      expect(res[0]).toMatchObject({
+        latitude: 45.5209666,
+        longitude: -73.6107766,
+        country: 'Canada',
+        countryCode: 'CA',
+        state: 'Québec',
+        city: 'Montréal'
+      });
+    });
+
+    it('works with openstreammap params', async () => {
+      const res = await geocoder.geocode({
+        q: '1231 Av. Lajoie, Montreal',
+        limit: 2
+      });
+
+      expect(res).toHaveLength(2);
+      expect(res[0]).toBeDefined();
+      expect(res[0]).toMatchObject({
+        latitude: 45.5209666,
+        longitude: -73.6107766,
+        country: 'Canada',
+        countryCode: 'CA',
+        state: 'Québec',
+        city: 'Montréal'
+      });
+    });
+  });
+});
